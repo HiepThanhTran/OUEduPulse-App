3013340f63737273742f2f7406a47c9b7ee36c73
SongHau <songhau2772003@gmail,com>
2024-07-31 15:20:21 +0700

    finish bottom navigation

---
+++ app/src/main/java/com/fh/app_student_management/ui/BottomNavigation.java
@@ -0,0 +1,52 @@
+package com.fh.app_student_management.ui;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.fh.app_student_management.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BottomNavigation extends AppCompatActivity {
+    private BottomNavigationView bottomNav;
+    private FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+
+        bottomNav = findViewById(R.id.bottomNav);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.navHome) {
+                    loadFragment(new HomeFragment());
+                } else if (itemId == R.id.navProfile) {
+                    loadFragment(new ProfileFragment());
+                }
+
+                return true;
+            }
+        });
+
+        loadFragment(new HomeFragment());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/fh/app_student_management/ui/HomeFragment.java
@@ -0,0 +1,19 @@
+package com.fh.app_student_management.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fh.app_student_management.R;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
