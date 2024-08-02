366c9929cbf6ba1fda7d3b7fb25347f1ac0a63ba
SongHau <songhau2772003@gmail,com>
2024-08-02 17:09:39 +0700

    finish edit profile screen

--- app/src/main/java/com/fh/app_student_management/ui/EditProfileActivity.java
+++ app/src/main/java/com/fh/app_student_management/ui/EditProfileActivity.java
@@ -1,20 +1,58 @@
 package com.fh.app_student_management.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.fh.app_student_management.R;
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.material.imageview.ShapeableImageView;
 
 public class EditProfileActivity extends AppCompatActivity {
+    private ShapeableImageView avatar;
+    private ImageView iconCamera;
+    private ImageView btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        avatar = findViewById(R.id.avatar);
+        iconCamera = findViewById(R.id.iconCamera);
+        btnBack = findViewById(R.id.btnBack); // Thêm dòng này
+
+        iconCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.with(EditProfileActivity.this)
+                        .crop()
+                        .compress(1024)
+                        .maxResultSize(1080, 1080)
+                        .start();
+            }
+        });
+
+        // Đặt OnClickListener cho btnBack
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Kết thúc Activity hiện tại
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            avatar.setImageURI(uri);
+        }
     }
 }
