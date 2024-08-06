d7fb9a3b03c78eaff58395b0ce46fb83b850288d
SongHau <songhau2772003@gmail,com>
2024-08-06 17:10:56 +0700

    finish lecturer management

---
+++ app/src/main/java/com/fh/app_student_management/ui/DeleteLecturerActivity.java
@@ -0,0 +1,4 @@
+package com.fh.app_student_management.ui;
+
+public class DeleteLecturerActivity {
+}
--- app/src/main/java/com/fh/app_student_management/ui/UserManagementActivity.java
+++ app/src/main/java/com/fh/app_student_management/ui/UserManagementActivity.java
@@ -4,16 +4,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.fh.app_student_management.R;
 
-public class ClassActivity extends AppCompatActivity {
+public class UserManagementActivity extends AppCompatActivity {
     private ImageView btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_class);
+        setContentView(R.layout.activity_user_management);
 
         btnBack = findViewById(R.id.btnBack);
         btnBack.setOnClickListener(new View.OnClickListener() {
