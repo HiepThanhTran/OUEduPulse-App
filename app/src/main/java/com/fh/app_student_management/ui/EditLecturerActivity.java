28a62ad572af5ba40227056c8b4043e8acc062f7
SongHau <songhau2772003@gmail,com>
2024-08-06 18:34:51 +0700

    fix lecturer management

--- app/src/main/java/com/fh/app_student_management/ui/EditLecturerActivity.java
+++ app/src/main/java/com/fh/app_student_management/ui/EditLecturerActivity.java
@@ -1,22 +1,53 @@
 package com.fh.app_student_management.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.fh.app_student_management.R;
 
 public class EditLecturerActivity extends AppCompatActivity {
+    private ImageView btnBack;
+    private EditText inputRole;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_lecturer);
 
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
+
+        inputRole = findViewById(R.id.inputRole);
+
+        inputRole.setText("Giảng viên");
+
+        inputRole.setFocusable(false);
+        inputRole.setFocusableInTouchMode(false);
+        inputRole.setClickable(true);
+
+        inputRole.setOnClickListener(v -> showRoleSelectionDialog());
+    }
+
+    private void showRoleSelectionDialog() {
+        final String[] roles = {"Giảng viên", "Admin"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Chọn vai trò")
+                .setItems(roles, (dialog, which) -> {
+                    inputRole.setText(roles[which]);
+                })
+                .create()
+                .show();
     }
 }
